9fc6b40db032bf6e4a68e033b45d8323f6a3f51a
ManpowerTracking <asithaindrajithk9@gmail.com>
2018-06-29 21:27:06 +0530

    new

--- login.php
+++ login.php
@@ -1,66 +1,37 @@
 <?php
+
 session_start();
 
-if(isset($_POST['submitWL'])){//check submit button is clicked
+if(isset($_SESSION['userType'])) {
+	header("location: ../index.php");
+}
+if(isset($_POST['submit'])){//check submit button is clicked
     require_once('db_config/config.php');//get databse connection
     //get name and password data
-    $username=$_POST['nameWL'];
-    $password=$_POST['pwdWL'];
+    $username=$_POST['name'];
+    $password=$_POST['pwd'];
     
     //hash the password to match with database password
     $hashed_password=hash('sha512',$password);
     
     //sql query
-    $query="SELECT * FROM worker_personal WHERE workerPhone=$username AND workerPassword=$hashed_password OR workerEmail=$username AND workerPassword=$hashed_password LIMIT 1";
+    $query="SELECT * FROM users WHERE userID=$username AND userPassword=$hashed_password LIMIT 1";
     
     //exucte sql query
     $result=mysqli_query($conn,$query);
     //check whther it returns results
     if(mysqli_num_rows($result)>0){
-        header("location: worker.php");
+        $userlogin = mysqli_fetch_array($userquery,MYSQLI_ASSOC);
+        $_SESSION['userType']=$userlogin["userType"];
+        $_SESSION['userID']=$userlogin["userID"];
+        if($userlogin["userType"]==1){
+            header("location: error.php");
+        }
     }
     else{
         header("location: error.php");
     }
 
-}
-
-if(isset($_POST['submitSL'])){
-    require_once('db_config/config.php');
-    $username=$_POST['nameSL'];
-    $password=$_POST['pwdSL'];
-
-    $hashed_password=hash('sha512',$password);
-    
-    $query="SELECT * FROM supplier_personal WHERE supPhone=$username AND supPassword=$password OR supEmail=$username AND supPassword=$password LIMIT 1";
-
-    $result=mysqli_query($conn,$query);
-
-    if(mysqli_num_rows($result)>0){
-        header("location: supplier.php");
-    }
-    else{
-        header("location: error.php");
-    }
-}
-
-if(isset($_POST['submitCL'])){
-    require_once('db_config/config.php');
-    $username=$_POST['nameCL'];
-    $password=$_POST['pwdCL'];
-
-    $hashed_password=hash('sha512',$password);
-    
-    $query="SELECT * FROM company_personal WHERE comEmail=$username AND comPassword=$password";
-
-    $result=mysqli_query($conn,$query);
-
-    if(mysqli_num_rows($result)>0){
-        header("location: company.php");
-    }
-    else{
-        header("location: error.php");
-    }
 }
 
 
