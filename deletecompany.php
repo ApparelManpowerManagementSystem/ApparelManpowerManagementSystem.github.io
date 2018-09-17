2cf18508d488d0ce7d26073c6a6acb2bcb8868e7
Pasindu Madusanka <pasindusenerath@gmail.com>
2018-09-17 18:46:22 +0530

    delete data php files

---
+++ deletecompany.php
@@ -0,0 +1,66 @@
+<?php
+$connection = mysqli_connect("localhost","root","","newmanpower"); // Establishing Connection with Server
+
+if(isset($_POST['save'])){ // Fetching variables of the form which travels in URL
+   
+    $comID = $_POST['comID'];
+    
+	
+$query = "DELETE FROM company WHERE comID='$comID'";
+if(mysqli_query($connection,$query))
+{
+ echo "Data Deleted successfully...!!";
+}
+else{
+ echo "<p>Deletion Failed </p>";
+}
+}
+mysqli_close($connection); // Closing Connection with Server
+?>
+
+<!DOCTYPE html>
+<html>
+ <head>  
+     
+<title> Company </title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+   
+    </head>
+    
+    
+    <body>
+        
+<div class="container">
+     <h2>Delete Company Details</h2>
+<form method="POST" action="deletecompany.php">
+    
+    <div class="col-xs-6 col-lg-offset-2">
+    
+    <div class="form-group">
+        <label for="comID">Company ID:</label>
+            <input type="text" class="form-control" id="comID" placeholder="Enter Company ID" name="comID">
+    </div>
+       
+    
+    
+       <div class="form-row text-right">
+           
+        <div class="col-12">
+         <button type="button" class="btn btn-danger">Cancel</button>
+        <button type="submit" name="save" id="save" class="btn btn-success">Submit</button>
+       </div>
+    </div>
+ </div>
+        
+   
+ 
+</form>
+   
+        </div>
+        
+    </body>
+</html>
---
+++ deletecompanyjobs.php
@@ -0,0 +1,66 @@
+<?php
+$connection = mysqli_connect("localhost","root","","newmanpower"); // Establishing Connection with Server
+
+if(isset($_POST['save'])){ // Fetching variables of the form which travels in URL
+   
+    $jobID = $_POST['jobID'];
+    
+	
+$query = "DELETE FROM company_job WHERE jobID='$jobID'";
+if(mysqli_query($connection,$query))
+{
+ echo "Data Deleted successfully...!!";
+}
+else{
+ echo "<p>Deletion Failed </p>";
+}
+}
+mysqli_close($connection); // Closing Connection with Server
+?>
+
+<!DOCTYPE html>
+<html>
+ <head>  
+     
+<title> Company JObs </title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+   
+    </head>
+    
+    
+    <body>
+        
+<div class="container">
+     <h2>Delete Company jobs Details</h2>
+<form method="POST" action="deletecompanyjobs.php">
+    
+    <div class="col-xs-6 col-lg-offset-2">
+    
+    <div class="form-group">
+        <label for="jobID">Job ID:</label>
+            <input type="text" class="form-control" id="jobID" placeholder="Enter Job ID" name="jobID">
+    </div>
+       
+    
+    
+       <div class="form-row text-right">
+           
+        <div class="col-12">
+         <button type="button" class="btn btn-danger">Cancel</button>
+        <button type="submit" name="save" id="save" class="btn btn-success">Submit</button>
+       </div>
+    </div>
+ </div>
+        
+   
+ 
+</form>
+   
+        </div>
+        
+    </body>
+</html>
