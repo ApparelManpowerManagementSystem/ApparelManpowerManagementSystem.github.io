b17520056e48d9dd6a227354e8ac01f4c455f630
Tharushi Galappaththi <gntharu@gmail.com>
2018-11-18 23:11:56 +0530

    send email

---
+++ send email/contact-thans.php
@@ -0,0 +1,10 @@
+<?php
+include('./includes/header.php');
+?>
+<h1>Thank you</h1>
+<p>Thank you for contact our company. You hsould receive a reply shortly.</p>
+
+<?php
+include('./includes/footer.php')
+?>
+
