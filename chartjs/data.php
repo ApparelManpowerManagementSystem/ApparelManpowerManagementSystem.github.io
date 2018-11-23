636e571436c1cb4cabb47247648b35cd5b88edf2
Pasindu Madusanka <pasindusenerath@gmail.com>
2018-11-24 00:07:49 +0530

    updating generate report files

---
+++ chartjs/data.php
@@ -0,0 +1,29 @@
+<?php
+
+header('Content-Type:application/json');
+
+define('DB_HOST','127.0.0.1');
+define('DB_USERNAME','root');
+define('DB_PASSWORD','');
+define('DB_NAME','newmanpower');
+
+$mysqli =new mysqli(DB_HOST,DB_USERNAME,DB_PASSWORD, DB_NAME);
+
+if(!$mysqli){
+
+    die("Connection failed: ".$mysqli->error);
+}
+
+$query = sprintf("SELECT jobType,jobCount FROM supplier_job");
+
+$result= $mysqli->query($query);
+
+$data =array();
+
+foreach ($result as $row){
+    $data[]= $row;
+}
+$result->close();
+$mysqli->close();
+
+print json_encode($data);
